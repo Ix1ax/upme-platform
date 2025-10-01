ace124219a381bdd03f7f97e7758f725d8ec8fd3
Ilya Samsonov <ixlax@mail.ru>
2025-10-01 10:55:21 +0300

    feat(gateway): bootstrap with CORS and /auth route

---
+++ backend/services/gateway/api-gateway/src/test/java/ru/ixlax/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ixlax.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
