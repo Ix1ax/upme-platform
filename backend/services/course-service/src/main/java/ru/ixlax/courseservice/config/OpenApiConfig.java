716dfe4e6fe952e9bb7918bdea862076bf23aeb2
Ilya Samsonov <103361737+Ix1ax@users.noreply.github.com>
2025-10-29 16:35:46 +0300

    feat/course/service (#33)
    * feat(course): add dockerfile
    
    * feat(course): add OpenAPI and application.yml

---
+++ backend/services/course-service/src/main/java/ru/ixlax/courseservice/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package ru.ixlax.courseservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI api() {
+        final String bearer = "BearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UPME Course Service API")
+                        .version("v1")
+                        .description("TODO"))
+                .addSecurityItem(new SecurityRequirement().addList(bearer))
+                .components(new Components().addSecuritySchemes(
+                        bearer,
+                        new SecurityScheme()
+                                .name(bearer)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
+
