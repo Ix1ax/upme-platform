3dd250a2c5bbdd76aa2705bae7b78230e65992d4
Ilya Samsonov <ixlax@mail.ru>
2025-10-29 11:08:39 +0300

    feat(course): add skeleton course service

---
+++ backend/services/course-service/src/test/java/ru/ixlax/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ixlax.courseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
